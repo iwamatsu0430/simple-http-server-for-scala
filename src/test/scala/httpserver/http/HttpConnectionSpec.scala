5c08672e4ea88e06d5c7f642cf309c3dd04e7a05
iwamatsu0430 <tatsuya.iwamatsu@bizreach.co.jp>
2019-07-02 00:09:50 +0900

    add test case

--- src/test/scala/httpserver/http/HttpConnectionSpec.scala
+++ src/test/scala/httpserver/http/HttpConnectionSpec.scala
@@ -4,9 +4,29 @@ import java.io._
 import java.net.{ ServerSocket, Socket }
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
 import org.mockito.MockitoSugar
 
-class HttpConnectionSpec extends WordSpec with MustMatchers with MockitoSugar {
+class HttpConnectionSpec extends WordSpec with MustMatchers with ScalaFutures with MockitoSugar {
+
+  "accept" when {
+    "failed initialize" must {
+      "return Future.failed" in {
+        val mockServerSocket = mock[ServerSocket]
+        when(mockServerSocket.accept()) thenThrow new Exception("mock exception")
+        the [Exception] thrownBy { HttpConnection.accept(mockServerSocket).futureValue }
+      }
+    }
+
+    "success initialize" must {
+      "return Future[HttpConnection]" in {
+        val mockSocket = mock[Socket]
+        val mockServerSocket = mock[ServerSocket]
+        when(mockServerSocket.accept()) thenReturn mockSocket
+        HttpConnection.accept(mockServerSocket).futureValue mustBe HttpConnection(mockServerSocket)
+      }
+    }
+  }
 
   def createMockConnection() = {
     val mockSocket = mock[Socket]
